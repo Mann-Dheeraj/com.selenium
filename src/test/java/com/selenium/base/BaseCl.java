bea8d1af859c11623b42f328b84856d26ab75c49
Mann-Dheeraj <dheeraj.mann@hotmail.com>
2025-01-10 06:13:22 +0530

    Using headless browser

--- src/test/java/com/selenium/base/BaseCl.java
+++ src/test/java/com/selenium/base/BaseCl.java
@@ -1,33 +1,35 @@
 package com.selenium.base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseCl {
 
-	public WebDriver driver;
+    public WebDriver driver;
 
-	@BeforeClass
-	public void initilizeBrowser() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-	}
-	
-	@AfterClass
-	public void teardown() {
-		driver.close();
-		driver.quit();
-	}
-
+    @BeforeClass
+    public void initializeBrowser() {
+        WebDriverManager.chromedriver().setup();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Run in headless mode
+        options.addArguments("--disable-gpu"); // Disable GPU rendering
+        options.addArguments("--no-sandbox"); // Required for root privileges
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
+        
+        driver = new ChromeDriver(options); // Pass ChromeOptions to the driver
+        driver.manage().window().maximize();
+    }
+    
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
